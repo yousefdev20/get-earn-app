952f65638b75ca11bd225298bb90cc1c44c7c600
Yousef <47471228+yousefdev20@users.noreply.github.com>
2023-02-18 13:18:44 +0300

    First release

--- app/Http/Controllers/Client/Auth/LoginController.php
+++ app/Http/Controllers/Client/Auth/LoginController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)//: Response
+    public function login(Request $request): Response
     {
         if (Auth::attempt($request->only(['phone', 'password']))) {
             $user = Auth::user();
